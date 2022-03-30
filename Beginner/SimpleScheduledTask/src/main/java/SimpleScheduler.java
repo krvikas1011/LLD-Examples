38e016afeab0deed96be245902cadcc275365c87
Verma,Shubhaangi <Shubhaangi.Verma@cerner.com>
2022-03-30 10:58:50 +0530

    Addressed review comments

--- Beginner/SimpleScheduledTask/src/main/java/SimpleScheduler.java
+++ Beginner/SimpleScheduledTask/src/main/java/SimpleScheduler.java
@@ -1,26 +1,31 @@
-import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SimpleScheduler {
 
-    private void scheduleTask() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the message:");
-        String message = sc.next();
-        System.out.println("Enter the interval in seconds:");
-        int interval = sc.nextInt();
+    private static Logger logger = LogManager.getLogger(SimpleScheduler.class);
+
+    private void scheduleTask(String message, int interval) {
         while(true) {
             System.out.println(message);
             try {
                 Thread.sleep(interval * 1000L);
             } catch (InterruptedException e) {
+                logger.error("The thread was interrupted ", e);
                 System.exit(-1);
             }
         }
     }
 
     public static void main(String[] args) {
-        new SimpleScheduler().scheduleTask();
+        String message = args[0];
+        int interval = 0;
+        try {
+            interval = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            logger.error("The input given for interval is not integer ", e);
+            System.exit(-1);
+        }
+        new SimpleScheduler().scheduleTask(message, interval);
     }
-
-
 }
